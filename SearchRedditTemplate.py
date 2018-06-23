75a15c834906c657fbfdce6f717c6038b7810506
JoshTavasso <36725862+JoshTavasso@users.noreply.github.com>
2018-06-22 18:52:34 -0700

    Add files via upload

--- SearchRedditTemplate.py
+++ SearchRedditTemplate.py
@@ -1,20 +1,19 @@
 # Searching For Wanted Reddit posts
 
 '''
-A bot that searches through one or more 
-subreddits, looking for specified keywords
-in the subreddit submissions. If the keywords are 
-found, the post is sent to a specified user, using
-reddit's private message system.
+A bot that searches through one or more subreddits, 
+looking for specified keywords in the subreddit submissions. 
+If the keywords are found, the post is sent to a specified user, 
+using reddit's private message system. 
 
-The script begins by asking the user what reddit user
-to send the submissions to, then asks for subreddits to
-search in, then asks for keywords. Lastly, the user is
-asked how long the bot should sleep for after each search.
+The script begins by asking the user what reddit user to send the 
+submissions to, then asks for subreddits to search in, then asks 
+for keywords. Lastly, the user is asked how long the bot should 
+sleep for after each search. The bot then runs indefinitely 
+(unless the user terminates the program manually), searching 
+through the first 100 posts in the subreddit, and sleeping a 
+specified number of seconds after each search.
 
-The bot then runs indefinitely (unless the user 
-terminates the program manually), searching through 
-all of the posts on the subreddit every 10 seconds.
 '''
 
 '''
